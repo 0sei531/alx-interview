963beda2b174d7179f2018fd563ea9c96d7e5054
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-06-27 08:29:40 -1200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -19,12 +19,12 @@ def pascal_triangle(n):
     """
     if not isinstance(n, int):
         raise TypeError("n must be an integer")
-    
+
     if n <= 0:
         return []
 
     triangle = [[1]]
-    
+
     for row_index in range(1, n):
         row = [1]
         row.extend([triangle[row_index - 1][i - 1] + triangle[row_index - 1][i] for i in range(1, row_index)])
