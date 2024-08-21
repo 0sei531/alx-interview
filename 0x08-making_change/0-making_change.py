d9d0db79252582ab70fffe6cd1437ca4e461cdfa
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-08-21 04:52:04 -1200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
 """
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
+Determines the fewest number of coins needed to meet a given
+total using dynamic programming.
 """
 
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-    Return: fewest number of coins needed to meet total
-        - If total is 0 or less, return 0
-        - If total cannot be met by any number of coins you have, return -1
-        - Coins is a list of the values of the coins in your possession
-        - The value of a coin will always be an integer greater than 0
-        - You can assume you have an infinite number of each denomination of
-        coin in the list
+    Determines the fewest number of coins needed to meet a given total.
+
+    Parameters:
+        coins (list of int): The values of the coins in your possession.
+        total (int): The total amount of change to make.
+
+    Returns:
+        int: The fewest number of coins needed to make the change,
+              or -1 if the total change cannot be made with the given coins.
     """
     if total <= 0:
         return 0
-
-    newVal = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
+    if not coins:
         return -1
 
-    return store[total]
+    # Initialize the DP array with infinity, except dp[0] which should be 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Iterate through all amounts from 1 to total
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
